54f3d39a6907e0d2c6b7109333dc7c12d305091f
Akshaya Rajasekaran <akshuraj284@gmail.com>
2023-11-06 12:24:33 +0530

    group list api integration

--- src/components/Products/GroupsSideBar/GroupsSideBar.jsx
+++ src/components/Products/GroupsSideBar/GroupsSideBar.jsx
@@ -1,17 +1,26 @@
+import { useSelector } from "react-redux";
 import s from "./GroupsSideBar.module.scss";
-import cx from "classnames"
+import cx from "classnames";
 
 export default function GroupsSideBar() {
-    return (
-        <div>
-            <div className={s.item}>
-                <div className={cx(s.titleBold, "pb-2")}>Group name 1</div>
-                <div className={cx(s.titleSmall1, "pb-1")}>Total no. of experiments&nbsp;&nbsp;<b>15</b></div>
-                <div className={cx("d-flex align-items-center gap-2")}>
-                    <div className={s.avatar}>AH</div>
-                    <div className={s.smallText}>Allison Herwitz</div>
-                </div>
+  const list = useSelector((state) => state?.products?.groupList);
+
+  return (
+    <div>
+      {list.map((item, index) => {
+        return (
+          <div className={s.item} key={index}>
+            <div className={cx(s.titleBold, "pb-2")}>{item.group_name}</div>
+            <div className={cx(s.titleSmall1, "pb-1")}>
+              Total no. of experiments&nbsp;&nbsp;<b>0</b>
             </div>
-        </div>
-    )
+            <div className={cx("d-flex align-items-center gap-2")}>
+              <div className={s.avatar}>AH</div>
+              <div className={s.smallText}>Allison Herwitz</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/store/features/products.js
+++ src/store/features/products.js
@@ -1,45 +1,76 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { client } from '../../utils/client';
+import { createSlice } from "@reduxjs/toolkit";
+import { client } from "../../utils/client";
 
 export const counterSlice = createSlice({
-    name: 'products',
-    initialState: {
-        value: 0,
-        groupList: []
+  name: "products",
+  initialState: {
+    value: 0,
+    groupList: [],
+  },
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
     },
-    reducers: {
-        increment: state => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
-            state.value += 1
-        },
-        decrement: state => {
-            state.value -= 1
-        },
-        incrementByAmount: (state, action) => {
-            state.value += action.payload
-        },
-        updateGroupList: (state, action) => {
-            state.groupList = action.payload
-        }
-    }
-})
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    updateGroupList: (state, action) => {
+      state.groupList = action.payload;
+    },
+    updateTotalGroup: (state, action) => {
+        state.totalGroup = action.payload;
+      },
+  },
+});
 
-export const { increment, decrement, incrementByAmount, updateGroupList } = counterSlice.actions
+export const { increment, decrement, incrementByAmount, updateGroupList } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
 
-export const incrementAsync = amount => dispatch => {
-    dispatch(incrementByAmount(amount))
-}
-
+export const incrementAsync = (amount) => (dispatch) => {
+  dispatch(incrementByAmount(amount));
+};
 
 export const initializeProductPage = () => async (dispatch, getState) => {
-    // const data = api call
-    // lenght, status
-    const data = await client.post("/view_by_user_id", {body : {user_id : 1}});
-    console.log(data)
-    dispatch(updateGroupList([]))
-}
+  // const data = api call
+  // lenght, status
+  //   const { status, data } = await client.post("/view_by_user_id", {
+  //     body: { user_id: 1 },
+  //   });
+  // const groupList = status ? data?.result || [];
+  const groupList = [
+    {
+      added_by: "1",
+      date_added: "Fri, 03 Nov 2023 11:04:08 GMT",
+      date_modified: "Fri, 03 Nov 2023 12:02:23 GMT",
+      group_id: 1,
+      group_name: "Group A",
+      is_deleted: false,
+      modified_by: "1",
+      user_id: "1",
+    },
+    {
+      added_by: "1",
+      date_added: "Mon, 06 Nov 2023 05:07:30 GMT",
+      date_modified: null,
+      group_id: 3,
+      group_name: "Group 1",
+      is_deleted: false,
+      modified_by: null,
+      user_id: "1",
+    },
+  ];
+  const totalGroup = 12; // 
+
+//   console.log(data);
+  dispatch(updateGroupList(groupList));
+//   dispatch(updateGroupList(totalGroup));
+};
