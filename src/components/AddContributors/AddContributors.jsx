1ada0ac7b35665e65b5045f6723901a5eae91023
Akshaya Rajasekaran <akshuraj284@gmail.com>
2023-11-07 15:35:47 +0530

    add contributors api integrated

--- src/components/AddContributors/AddContributors.jsx
+++ src/components/AddContributors/AddContributors.jsx
@@ -3,11 +3,15 @@ import s from "./AddContributors.module.scss";
 import { ReactSVG } from "react-svg";
 import addUserIcon from "../../assets/svg/addUser.svg";
 import { useState, useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import SearchInput from "../common/SearchInput/SearchInput";
+import { getContributorsList } from "../../store/features/products";
 
 export default function AddContributors() {
     const [addContributors, setAddContributors] = useState(false);
     const popupRef = useRef(null);
+
+    const dispatch = useDispatch();
 
     useEffect(() => {
         function handleClickOutside(event) {
@@ -23,26 +27,63 @@ export default function AddContributors() {
         };
     }, []);
 
+    useEffect(() => {
+        dispatch(getContributorsList());
+    }, [])
+
+    const contributorsData = useSelector((state) => state.products.contributorsData);
+    console.log(contributorsData, "contributorsData");
+
+
+    function getInitials(fullName) {
+        const name = fullName.split(' ');
+        if (name.length >= 2) {
+            return name[0][0] + name[1][0];
+        } else if (name.length === 1) {
+            return name[0][0];
+        } else {
+            return '';
+        }
+    }
+
     return (
         <div className="mb-5">
             <div className={cx(s.title2, "pt-5")}>Contributors</div>
             <p className={cx(s.text, "pt-2")}>Placeholder text comes here....</p>
             <div
-                className={cx("d-flex gap-2 align-items-center", s.linkTextPrimary)}
-                onClick={() => setAddContributors(true)}
+                className={cx("d-flex gap-2 align-items-center", s.linkTextPrimary, s.popupText)}
+                onClick={() => setAddContributors(!addContributors)}
             >
                 <ReactSVG src={addUserIcon} />
                 Add contributors
+
+                {addContributors && (
+                    <div className={cx(s.popup, s.addContributorPopup)} ref={popupRef}>
+                        <div>
+                            <div className={cx(s.addConText)}>Add Contributor</div>
+                            <div className={cx("px-2")}>
+                                <SearchInput placeholder="Search" />
+                            </div>
+                        </div>
+                        <div className="overflow-y-auto">
+                            {contributorsData?.length > 0 && contributorsData.map((item) => (
+                                <>
+                                    <div className="d-flex pt-3 ps-2 align-items-center">
+                                        <div><input type="checkbox" className={cx(s.contributorCheckbox, "mx-2")} /></div>
+                                        <div className={cx(s.userNameAvatar, "mx-2")}>{getInitials(item.contributor_name)}</div>
+                                        <div>{item.contributor_name}</div>
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+                        <div className={s.buttons}>
+                            <button>Clear</button>
+                            <button type="submit" className="text-white">Apply</button>
+                        </div>
+                    </div>
+                )}
+
             </div>
-            {addContributors && (
-                <div className={cx(s.addContributorPopup)} ref={popupRef}>
-                    <span className={cx(s.addConText)}>Add Contributor</span>
-                    <div className={cx("pt-3 px-2")}>
-                    <SearchInput placeholder="Search" />
-                </div>
-                </div>
-                
-            )}
         </div>
     );
 }
--- src/store/features/products.js
+++ src/store/features/products.js
@@ -2,123 +2,132 @@ import { createSlice } from "@reduxjs/toolkit";
 import { client } from "../../utils/client";
 
 export const counterSlice = createSlice({
-    name: "products",
-    initialState: {
-        value: 0,
-        groupList: [],
-        selectedGroup: null,
-        totalGroup: 0,
-        isAddGroup: false,
-        experimentData: [],
+  name: "products",
+  initialState: {
+    value: 0,
+    groupList: [],
+    selectedGroup: null,
+    totalGroup: 0,
+    isAddGroup: false,
+    experimentData: [],
+    contributorsData: [],
+  },
+  reducers: {
+    increment: (state) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.value += 1;
     },
     reducers: {
-        increment: (state) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
-            state.value += 1;
-        },
-        reducers: {
-            increment: (state) => {
-                // Redux Toolkit allows us to write "mutating" logic in reducers. It
-                // doesn't actually mutate the state because it uses the immer library,
-                // which detects changes to a "draft state" and produces a brand new
-                // immutable state based off those changes
-                state.value += 1;
-            },
-            decrement: (state) => {
-                state.value -= 1;
-            },
-            incrementByAmount: (state, action) => {
-                state.value += action.payload;
-            },
-            updateGroupList: (state, action) => {
-                state.groupList = action.payload;
-            },
-            updateTotalGroupNum: (state, action) => {
-                state.totalGroup = action.payload;
-            },
-            updateSelectedGroup: (state, action) => {
-                state.selectedGroup = action.payload;
-            },
-            updateAddGroupPopupStatus: (state, action) => {
-                state.isAddGroup = action.payload;
-            },
-        },
-        incrementByAmount: (state, action) => {
-            state.value += action.payload;
-        },
-        updateGroupList: (state, action) => {
-            state.groupList = action.payload;
-        },
-        updateTotalGroupNum: (state, action) => {
-            state.totalGroup = action.payload;
-        },
-        updateSelectedGroup: (state, action) => {
-            state.selectedGroup = action.payload;
-        },
-        updateAddGroupPopupStatus: (state, action) => {
-            state.isAddGroup = action.payload;
-        },
-        updateExperimentData: (state, action) => {
-            state.experimentData = action.payload;
-        },
+      increment: (state) => {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        state.value += 1;
+      },
+      decrement: (state) => {
+        state.value -= 1;
+      },
+      incrementByAmount: (state, action) => {
+        state.value += action.payload;
+      },
+      updateGroupList: (state, action) => {
+        state.groupList = action.payload;
+      },
+      updateTotalGroupNum: (state, action) => {
+        state.totalGroup = action.payload;
+      },
+      updateSelectedGroup: (state, action) => {
+        state.selectedGroup = action.payload;
+      },
+      updateAddGroupPopupStatus: (state, action) => {
+        state.isAddGroup = action.payload;
+      },
     },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    updateGroupList: (state, action) => {
+      state.groupList = action.payload;
+    },
+    updateTotalGroupNum: (state, action) => {
+      state.totalGroup = action.payload;
+    },
+    updateSelectedGroup: (state, action) => {
+      state.selectedGroup = action.payload;
+    },
+    updateAddGroupPopupStatus: (state, action) => {
+      state.isAddGroup = action.payload;
+    },
+    updateExperimentData: (state, action) => {
+      state.experimentData = action.payload;
+    },
+    updateContributorsData: (state, action) => {
+      state.contributorsData = action.payload;
+    },
+  },
 });
 
 export const {
-    increment,
-    decrement,
-    incrementByAmount,
-    updateGroupList,
-    updateSelectedGroup,
-    updateTotalGroupNum,
-    updateAddGroupPopupStatus,
-    updateExperimentData,
+  increment,
+  decrement,
+  incrementByAmount,
+  updateGroupList,
+  updateSelectedGroup,
+  updateTotalGroupNum,
+  updateAddGroupPopupStatus,
+  updateExperimentData,
+  updateContributorsData,
 } = counterSlice.actions;
 
 export default counterSlice.reducer;
 
 export const incrementAsync = (amount) => (dispatch) => {
-    dispatch(incrementByAmount(amount));
+  dispatch(incrementByAmount(amount));
 };
 
 export const getGroupData = () => async (dispatch, getState) => {
-    const { status, data } = await client.post("/view_by_user_id", {
-        user_id: 1,
-    });
-    const groupList = status ? data?.results : [];
-    const totalGroup = data?.total_groups || 0;
+  const { status, data } = await client.post("/view_by_user_id", {
+    user_id: 1,
+  });
+  const groupList = status ? data?.results : [];
+  const totalGroup = data?.total_groups || 0;
 
-    dispatch(updateGroupList(groupList));
-    dispatch(updateTotalGroupNum(totalGroup));
-}
+  dispatch(updateGroupList(groupList));
+  dispatch(updateTotalGroupNum(totalGroup));
+};
 
 export const initializeProductPage = () => async (dispatch, getState) => {
-    await dispatch(getGroupData());
-    const state = getState()
-    const fristGroupId = state?.products?.groupList?.[0]?.group_id
-    dispatch(getExperimentsByGroupId(fristGroupId))
-
+  await dispatch(getGroupData());
+  const state = getState();
+  const fristGroupId = state?.products?.groupList?.[0]?.group_id;
+  dispatch(getExperimentsByGroupId(fristGroupId));
 };
 
 export const getExperimentsByGroupId =
-    (groupId) => async (dispatch, getState) => {
-        dispatch(updateSelectedGroup(groupId));
-        const { status, data } = await client.post("/get_experiments_by_group_id", {
-            group_id: groupId,
-        });
+  (groupId) => async (dispatch, getState) => {
+    dispatch(updateSelectedGroup(groupId));
+    const { status, data } = await client.post("/get_experiments_by_group_id", {
+      group_id: groupId,
+    });
 
-        const experimentData = status ? data.results : [];
-        dispatch(updateExperimentData(experimentData));
-    };
+    const experimentData = status ? data.results : [];
+    dispatch(updateExperimentData(experimentData));
+  };
 
 export const createGroup = (group_name) => async (dispatch, getState) => {
-    dispatch(updateAddGroupPopupStatus(false));
-    const { status, data } = await client.post("/add_group", {
-        group_name,
-        user_id: 1,
-    });
-    dispatch(getGroupData());
+  dispatch(updateAddGroupPopupStatus(false));
+  const { status, data } = await client.post("/add_group", {
+    group_name,
+    user_id: 1,
+  });
+  dispatch(getGroupData());
+};
+export const getContributorsList = () => async (dispatch, getState) => {
+  const { status, data } = await client.post("/get_contributors");
+  const contributorsData = status ? data.results : [];
+  dispatch(updateContributorsData(contributorsData));
 };
