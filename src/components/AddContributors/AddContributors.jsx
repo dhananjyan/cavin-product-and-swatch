9d98b7154207276af754db059a5a859abaec1eec
Akshaya Rajasekaran <akshuraj284@gmail.com>
2023-11-07 17:51:08 +0530

    search updated

--- src/components/AddContributors/AddContributors.jsx
+++ src/components/AddContributors/AddContributors.jsx
@@ -9,6 +9,7 @@ import { getContributorsList } from "../../store/features/products";
 
 export default function AddContributors() {
     const [addContributors, setAddContributors] = useState(false);
+    const [search, setSearch] = useState("");
     const popupRef = useRef(null);
 
     const dispatch = useDispatch();
@@ -29,60 +30,68 @@ export default function AddContributors() {
 
     useEffect(() => {
         dispatch(getContributorsList());
-    }, [])
-
+    }, []);
     const contributorsData = useSelector((state) => state.products.contributorsData);
-    console.log(contributorsData, "contributorsData");
-
-
     function getInitials(fullName) {
-        const name = fullName.split(' ');
+        const name = fullName.split(" ");
         if (name.length >= 2) {
             return name[0][0] + name[1][0];
         } else if (name.length === 1) {
             return name[0][0];
         } else {
-            return '';
+            return "";
         }
     }
+    const filteredContributors = contributorsData.filter((item) =>
+        item.contributor_name.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <div className="mb-5">
             <div className={cx(s.title2, "pt-5")}>Contributors</div>
             <p className={cx(s.text, "pt-2")}>Placeholder text comes here....</p>
-            <div
-                className={cx("d-flex gap-2 align-items-center", s.linkTextPrimary, s.popupText)}
-                onClick={() => setAddContributors(!addContributors)}
-            >
+            <div className={cx("d-flex gap-2 align-items-center", s.popupText)}>
                 <ReactSVG src={addUserIcon} />
-                Add contributors
-
+                <span className={s.linkTextPrimary} onClick={() => setAddContributors(true)}>
+                    Add contributors
+                </span>
                 {addContributors && (
                     <div className={cx(s.popup, s.addContributorPopup)} ref={popupRef}>
                         <div>
                             <div className={cx(s.addConText)}>Add Contributor</div>
                             <div className={cx("px-2")}>
-                                <SearchInput placeholder="Search" />
+                                <SearchInput
+                                    placeholder="Search"
+                                    value={search}
+                                    onChange={(e) => setSearch(e.target.value)}
+                                />
                             </div>
                         </div>
                         <div className="overflow-y-auto">
-                            {contributorsData?.length > 0 && contributorsData.map((item) => (
-                                <>
-                                    <div className="d-flex pt-3 ps-2 align-items-center">
-                                        <div><input type="checkbox" className={cx(s.contributorCheckbox, "mx-2")} /></div>
-                                        <div className={cx(s.userNameAvatar, "mx-2")}>{getInitials(item.contributor_name)}</div>
-                                        <div>{item.contributor_name}</div>
+                            {filteredContributors.length > 0 &&
+                                filteredContributors.map((item) => (
+                                    <div key={item.id} className="d-flex pt-3 ps-2 align-items-center">
+                                        <div>
+                                            <input
+                                                type="checkbox"
+                                                className={cx(s.contributorCheckbox, "mx-2")}
+                                            />
+                                        </div>
+                                        <div className={cx(s.userNameAvatar, "mx-2")}>
+                                            {getInitials(item.contributor_name)}
+                                        </div>
+                                        <div className={s.contributorName}>{item.contributor_name}</div>
                                     </div>
-                                </>
-                            ))}
+                                ))}
                         </div>
                         <div className={s.buttons}>
                             <button>Clear</button>
-                            <button type="submit" className="text-white">Apply</button>
+                            <button type="submit" className="text-white">
+                                Apply
+                            </button>
                         </div>
                     </div>
                 )}
-
             </div>
         </div>
     );
--- src/components/common/SearchInput/SearchInput.jsx
+++ src/components/common/SearchInput/SearchInput.jsx
@@ -4,11 +4,11 @@ import s from './SearchInput.module.scss';
 import searchIcon from "../../../assets/svg/search.svg"
 
 export default function SearchInput(props) {
-    const { placeholder } = props;
+    const { placeholder, value, onChange } = props;
     return (
         <div className={s.inputContainer}>
             <ReactSVG src={searchIcon} className={s.icon} />
-            <input className={s.input} placeholder={placeholder} />
+            <input className={s.input} placeholder={placeholder} value={value} onChange={onChange} />
         </div>
     )
 }
